bfa4c8e3c0bce27478188b0a449d772469172e3e
isabellahiggins <b00162371@mytudublin.ie>
2025-12-04 21:38:33 +0000

    Fixed register page to update mongodb with the new user

--- src/app/api/newregister/route.js
+++ src/app/api/newregister/route.js
@@ -1,30 +1,18 @@
+import {MongoClient} from 'mongodb';
+
 export async function GET(req) {
 
-
-  // Make a note we are on
-
-  // the api. This goes to the console.
-
+  // Make a note we are on the api. This goes to the console.
   console.log("in the api page")
 
-
-
-  // get the values
-
-  // that were sent across to us.
-
+  // get the values that were sent across to us.
   const { searchParams } = new URL(req.url)
 
   const email = searchParams.get('email')
-
-    const confirmEmail = searchParams.get('confirmEmail')
-
+  const confirmEmail = searchParams.get('confirmEmail')
   const pass = searchParams.get('pass')
-
-   const confirmPass = searchParams.get('confirmPass')
-
+  const confirmPass = searchParams.get('confirmPass')
   const tel = searchParams.get('telephoneNumber')
-
   const address = searchParams.get('address')
 
 
@@ -35,13 +23,41 @@ export async function GET(req) {
   console.log("Sent telephoneNumber" + tel);
   console.log("Sent address" + address);
 
- 
 
+if(email !== confirmEmail){
+  return Response.json({ success: false, message: "Incorrect Email" });
+}
 
-  // database call goes here
+if(pass != confirmPass){
+  return Response.json({ success: false, message: "Incorrect Password" });
+}
 
-  // at the end of the process we need to send something back.
+//if user enters an email already used
+  const url = "mongodb://root:example@localhost:27017/";
+  const client = new MongoClient(url);
+  await client.connect();
+  const dbName = "app";
+  const db = client.db(dbName);
+  const collection = db.collection("login");
 
-  return Response.json({ "data":"ok" })
+  const userExists = await collection.findOne({ username: email });
 
+  if(userExists){
+    return Response.json({ success: false, message: "Email already registered" });
+  }
+
+  //Create new user
+  const newUser = {
+    username: email,
+    pass: pass,
+    acctype: "customer",
+    telephone: tel,
+    address: address
+  };
+
+  const insertResult = await collection.insertOne(newUser);
+  console.log("New user created");
+
+  //database call goes here at the end of the process we need to send something back.
+  return Response.json({ success: true, message: "Registration completed successfuly" });
 }
--- src/app/newregister/page.js
+++ src/app/newregister/page.js
@@ -3,33 +3,25 @@
 //MUI framework imports that let us use components
 import * as React from 'react';
 
-import Avatar from '@mui/material/Avatar';
-
 import Button from '@mui/material/Button';
-
-
 import TextField from '@mui/material/TextField';
-
 import FormControlLabel from '@mui/material/FormControlLabel';
-
 import Checkbox from '@mui/material/Checkbox';
-
 import Link from '@mui/material/Link';
-
 import Container from '@mui/material/Container';
-
 import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
 
 
 //main function for the main, all code goes here
 export default function Home() {
 
+  const [message, setMessage] = React.useState('');
+  const [messageType, setMessageType] = React.useState('success');
+
 //Javascript
   const handleSubmit = (event) => {
-
-                
-
-  console.log("handling submit");
+    console.log("handling submit");
 
   //prevent defalt function call is to make sure browser doesn't refresh the page
   event.preventDefault();
@@ -37,228 +29,133 @@ export default function Home() {
   //get data from form and store it into const dara
   const data = new FormData(event.currentTarget);
 
-
 //data.get to get the email and pass variables from the form data
-   let email = data.get('email')
+  let email = data.get('email')
+  let confirmEmail = data.get('confirmEmail')
+  let pass = data.get('pass')
+  let confirmPass = data.get('confirmPass')
+  let address = data.get('address')
+  let telephoneNumber = data.get('telephoneNumber')
 
-   let pass = data.get('pass')
-
-   let address = data.get('address')
-
-    let telephoneNumber = data.get('telephoneNumber')
-
-
-
-
-   console.log("Sent email:" + email)
-
-   console.log("Sent pass:" + pass)
-
-   console.log("Sent address:" + address)
-
-   console.log("Sent telephoneNumber:" + telephoneNumber)
-
+//print the sent data to console
+  console.log("Sent email:" + email)
+  console.log("Sent confirmEmail:" + confirmEmail)
+  console.log("Sent pass:" + pass) 
+  console.log("Sent address:" + address)
+  console.log("Sent telephoneNumber:" + telephoneNumber)
 
 // run a function to talk to the database
   runDBCallAsync(
-  `/api/newregister?email=${email}&confirmEmail=${confirmEmail}&pass=${pass}&confirmPass=${confirmPass}&address=${address}&telephoneNumber=${telephoneNumber}`
-);
-
-
-
-
-
+  `/api/newregister?email=${email}&confirmEmail=${confirmEmail}&pass=${pass}&confirmPass=${confirmPass}&address=${address}&telephoneNumber=${telephoneNumber}`);
  }; // end handle submit
-
 
 //async means it will run by itself and not hold up other code
 async function runDBCallAsync(url) {
 
-
-
     const res = await fetch(url);
-
     const data = await res.json();
 
-
- 
-
-    if(data.data== "valid"){
-
-      console.log("login is valid!")
-
-
-     
-
-    } else {
-
-
-      console.log("not valid  ")
-
-    }
-
+  if(data.success){
+    setMessage(data.message);
+    setMessageType('success');
+  }else{
+    setMessage(data.message);
+    setMessageType('error');
   }
-
-
+}
 
 //html code
   return (
 
     <Container maxWidth="sm">
-
     <Box sx={{ height: '100vh' }} >
-
+      {message && <Alert severity={messageType}>{message}</Alert>}
 
     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
 
-    <TextField
-
-      margin="normal"
-
+    <TextField margin="normal"
       required
-
       fullWidth
-
       id="email"
-
       label="Email Address"
-
       name="email"
-
       autoComplete="email"
-
       autoFocus
-
     />
 
     <TextField
-
       margin="normal"
-
       required
-
       fullWidth
-
       name="confirmEmail"
-
       label="Confirm your email"
-
       type="email"
-
       id="confirmEmail"
-
       autoComplete="current-email"
       ></TextField>
 
     <TextField
-
       margin="normal"
-
       required
-
       fullWidth
-
       name="pass"
-
       label="Pass"
-
       type="password"
-
       id="pass"
-
       autoComplete="current-password"
       ></TextField>
 
       <TextField
-
       margin="normal"
-
       required
-
       fullWidth
-
       name="confirmPass"
-
       label="confirm password"
-
       type="password"
-
       id="confirmPass"
-
       autoComplete="current-password"
       ></TextField>
 
-
       <TextField
-
       margin="normal"
-
       required
-
       fullWidth
-
       name="address"
-
       label="Address"
-
       type="text"
-
       id="address"
-
       autoComplete="current-address"
       ></TextField>
 
       <TextField
-
       margin="normal"
-
       required
-
       fullWidth
-
       name="telephoneNumber"
-
       label="Telephone Number"
-
       type="tel"
-
       id="telephoneNumber"
-
       autoComplete="current-telephoneNumber"
       ></TextField>
 
-    
-
  <FormControlLabel
       control={<Checkbox value="remember" color="primary" />}
-
       label="Remember me"
 ></FormControlLabel>
 
 
     <Button
-
       type="submit"
-
       fullWidth
-
       variant="contained"
-
       sx={{ mt: 3, mb: 2 }}
-
     >
-
-      Sign In
-
+      Register
     </Button>
-
-</Box>
-
-</Box>
-
-       </Container>
+    </Box>
+  </Box>
+</Container>
 
   ); // end return
-
 }
 
